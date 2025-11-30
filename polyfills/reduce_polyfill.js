803a67047e241e45ef51f982db39e3e6955c40d7
Zeeshan Ahmad <zeeshan>
2025-11-30 12:40:57 +0530

    reduce polyfill

---
+++ polyfills/reduce_polyfill.js
@@ -0,0 +1,29 @@
+
+
+
+Array.prototype.customReduce = function(callback, initialValue) {
+    if (this == null || this == undefined || this.length < 0) {
+        throw new Error("null or undefined reduce");
+    }
+    if (typeof callback !== 'function') {
+        throw new Error("callback is not a function");
+    }
+
+    const arr = this;
+    let accumulator = initialValue !== undefined ? initialValue : arr[0];
+
+
+    //its beacuse we have already used first element as initial value in above line
+    let startIndex = initialValue !== undefined ? 0 : 1; 
+
+    for (let i = startIndex; i < arr.length; i++) {
+        accumulator = callback(accumulator, arr[i], i, arr);
+    }
+
+    return accumulator;
+}
+
+// Example usage:
+
+const numbers = [1, 2, 3, 4];
+const sum = numbers.customReduce((acc, curr) => acc + curr, 0);
