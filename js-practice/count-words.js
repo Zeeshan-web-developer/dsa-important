b16e0af4e3cd66c0a9059688fffbc65a198af963
Zeeshan Ahmad <zeeshan>
2026-03-28 09:22:32 +0530

    largest word

--- js-practice/count-words.js
+++ js-practice/count-words.js
@@ -7,3 +7,13 @@ function countWords(str) {
     let words = str.trim().split(/\s+/)
     return words.length
 }
+
+function largestword(str) {
+    let words = str.trim().split(/\s+/)
+    let largest = ""
+    for(let word of words){
+        if(word.length > largest.length){
+            largest = word
+        }
+    }
+}
