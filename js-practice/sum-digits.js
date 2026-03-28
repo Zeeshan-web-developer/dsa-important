52a7894f1360090e4a8951287235eae556c7edad
Zeeshan Ahmad <zeeshan>
2026-03-28 09:25:57 +0530

    sum of digits

---
+++ js-practice/sum-digits.js
@@ -0,0 +1,12 @@
+//sum of digits
+
+
+function digits(num) {
+    let sum = 0;
+    while (num > 0) {
+        sum += num % 10
+        num = Math.floor(num / 10)
+        
+    }
+    return sum;
+}
